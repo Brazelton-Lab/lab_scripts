2545654bdf2d06dc249fac651b9e8759d5fc7dff
Christopher Thornton <christopher.n.thornton@gmail.com>
2015-05-28 09:50:45 -0600

    Accept sample coverage and abundance files instead of summary files

--- pathway_table.py
+++ pathway_table.py
@@ -7,6 +7,7 @@ abundance in a HumanN-processed sample.
 import sys
 import os
 import argparse
+import re
 
 def io_check(infile, mode='rU'):
     try:
@@ -16,93 +17,127 @@ def io_check(infile, mode='rU'):
         sys.exit(str(e))
     return infile
 
-def main():	
-    path_table = {}
-    with open(cov_file, 'rU') as fh:
-        header = fh.readline()
-        for line in fh:
-            path_id, coverage = line.strip().split('\t')
-            coverage = float(coverage)
-            if '|' in path_id:
-                continue
-            if coverage >= threshold:
-                path_table[path_id] = {'coverage': 0, 
-                                       'abundance': 0,
-                                       'pathway': []}
-                path_table[path_id]['coverage'] = coverage
-		
-    with open(abund_file, 'rU') as fh:
-        header = fh.readline()
-        for line in fh:
-            path_id, abundance = line.strip().split('\t')
-            abundance = float(abundance)
-            if '|' in path_id:
-                continue
+def parse_file(infile, path_table, type):
+    file_base = os.path.basename(infile)
+    sample_name = file_base.split('.')[0]
+    with open(infile, 'rU') as in_h:
+        header = in_h.readline()
+        if (type not in header.lower()):
+            print("error: incorrect header format in {}".format(file_base))
+            sys.exit(1)
+        for line in in_h:
+            line = line.strip().split('\t')
+            path_id = line[0]
+            value = line[1]
             try:
-                path_table[path_id]['abundance'] = abundance
-            except KeyError as e:
-                print("warning: {} in abundance file not in coverage file".format(str(e)))
-
-    with open(foam_file, 'rU') as fh:
-        header = fh.readline()
-        for line in fh:
-            col = line.strip().split('\t')
-            path_id = col[0]
-            pathway = col[1:]
-            try:
-                path_table[path_id]['pathway'] = pathway
+                path_table[path_id][sample_name][type] = value
             except KeyError:
-                continue
+                try:
+                    path_table[path_id][sample_name]= {type: value}
+                except KeyError:
+                    path_table[path_id] = {sample_name: {type: value}}
+    return path_table
 
-    out_h = open(out_file, 'w')
-    krona_h = open(krona_file, 'w')
+def main():
+    path_table = {}
+    directory = args.directory
+    match_cov = re.compile(".+_04a.+-mpt-.+")
+    match_abund = re.compile(".+_04b.+-mpt-.+")
+    infiles = []
+    samples = []
+    for root, dirs, files in os.walk(directory):
+        for infile in files:
+            sample_name = infile.split('.')[0]
+            if match_cov.match(infile) or match_abund.match(infile):
+                if args.selected:
+                    if sample_name in args.selected:
+                        infiles.append(os.path.join(root, infile))
+                        if sample_name not in samples:
+                            samples.append(sample_name)
+                else:
+                    infiles.append(os.path.join(root, infile))
+                    if sample_name not in samples:
+                        samples.append(sample_name)
+    if not infiles:
+        print("error: unable to locate coverage and abundance files in directory \"{}\"".format(directory))
+        sys.exit(1)
 
-    output = 'Pathway\tCoverage\tAbundance\n'
-    out_h.write(output)
-    for path_id in path_table:
-        abundance = str(path_table[path_id]['abundance'])
-        coverage = str(path_table[path_id]['coverage'])
-        if foam_file:
-            out_path = '\t'.join(path_table[path_id]['pathway'])
-        else:
+    if args.prefix:
+        prefix = args.prefix
+    else:
+        prefix = "pathways"
+    out_abund = io_check("{}.abundance.csv".format(prefix), 'w')
+    out_cov = io_check("{}.coverage.csv".format(prefix), 'w')
+
+    for infile in infiles:
+        if match_cov.match(infile):
+            type = "coverage"
+        elif match_abund.match(infile):
+            type = "abundance"
+        path_table = parse_file(infile, path_table, type)
+
+    if args.foam:
+        path_map = {}
+        with open(foam_file, 'rU') as fh:
+            header = fh.readline()
+            for line in fh:
+                col = line.strip().split('\t')
+                path_id = col[0]
+                pathway = col[1:]
+                path_map[path_id] = ';'.join(pathway)
+
+    cov_h = open(out_cov, 'w')
+    abund_h = open(out_abund, 'w')
+
+    header = "Pathway\t{}\n".format('\t'.join(sorted(samples)))
+    cov_h.write(header)
+    abund_h.write(header)
+    for path_id in sorted(path_table):
+        out_cov = []
+        out_abund = []
+        try:
+            out_path = path_map[path_id]
+        except (KeyError, NameError):
             out_path = path_id
-        output = "{}\t{}\t{}\n".format(out_path.replace('\t', ', '), coverage, abundance)
-        out_h.write(output)
-        output = "{}\t{}\n".format(abundance, out_path)
-        krona_h.write(output)
+        out_cov.append(out_path)
+        out_abund.append(out_path)
+        for sample in sorted(samples):
+            try:
+                coverage = path_table[path_id][sample]["coverage"]
+            except KeyError:
+                coverage = 0
+            try:
+                abundance = path_table[path_id][sample]["abundance"]
+            except KeyError:
+                abundance = 0
+            out_cov.append(str(coverage))
+            out_abund.append(str(abundance))
+        cov_h.write("{}\n".format('\t'.join(out_cov)))
+        abund_h.write("{}\n".format('\t'.join(out_abund)))
 
-    out_h.close()
-    krona_h.close()
+    cov_h.close()
+    abund_h.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Produce a table containing "
         "the metabolic pathways found in a sample through HUMAnN, along with "
-        "their coverage and abundance")
-    parser.add_argument('coverage', metavar='COVERAGE', 
-                        type=io_check, 
-                        help="pathway coverage file generated by HUMAnN")
-    parser.add_argument('abundance', metavar='ABUNDANCE',
-                        type=io_check,
-                        help="pathway abundance file generated by HUMAnN")
-    parser.add_argument('-f', '--foam', metavar='FOAM',
+        "the coverage and abundance of each pathway")
+    parser.add_argument('directory', metavar='DIR', 
+                        help="directory containing the coverage and abundance files from HUMAnN "
+                        "(files containing \"04a\" and \"04b\")")
+    parser.add_argument('-s', '--samples', metavar='PREFIX',
+                        nargs='*',
+                        dest='selected',
+                        help="include only samples with these prefixs. Each "
+                        "prefix provided should have a match in the coverage "
+                        "and abundance files [default: include all]")
+    parser.add_argument('-f', '--foam', metavar='FILE',
                         type=io_check,
                         help="file relating foam pathway ids to gene families")
-    parser.add_argument('-t', '--threshold', 
-                        type=float, 
-                        default = 0.3, 
-                        help = "coverage threshold [default: 0.3]")
-    parser.add_argument('-p', '--prefix', 
-                        required=True,
-                        type=str, 
-                        help = "prefix of output files")
+    parser.add_argument('-p', '--prefix', metavar='PREF',
+                        help="prefix for the output files")
     args = parser.parse_args()
-    threshold = args.threshold
     foam_file = args.foam
-    cov_file = args.coverage
-    abund_file = args.abundance
-    out_file = io_check('{}.csv'.format(args.prefix), 'w')
-    krona_file = io_check('{}.krona'.format(args.prefix), 'w')
-    
     main()
     sys.exit(0)
 
