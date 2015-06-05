a9709aee96bbb98da34e82cf6e2b125daa7b70bd
Christopher Thornton <christopher.n.thornton@gmail.com>
2015-06-04 18:02:34 -0600

    modify make-group-file-from-header.py so that it no longer cuts off a portion of a sample name

--- 16S/VAMPS_data/make-group-file-from-header.py
+++ 16S/VAMPS_data/make-group-file-from-header.py
@@ -2,33 +2,46 @@
 # sort sequences in standard.fasta according to bar code
 # files saved with filenames according to barcodes_distribution.txt
 
+from __future__ import print_function
 import sys
-infilename = sys.argv[1]
+import os
+import re
+import argparse
 
-outfilename = infilename + '.group'
-outfile = open(outfilename,'a')
-		
-fastas = open(infilename)
-for line in fastas:
-	#print line
-	if line[0] == '>':
-		header = line.strip('\n')
-		header = header.split(' ')
-		header = header[0]
-		words = line.split('|')
-		fasta_sample = words[2].split('_')
-		if len(fasta_sample) < 3: sample = fasta_sample
-		else: sample = fasta_sample[1:-1]
+def io_check(infile, mode='rU'):
+    try:
+        fh = open(infile, mode)
+    except IOError as e:
+        print(e)
+        sys.exit(1)
+    else:
+        fh.close()
+    return infile
 
-		outfile.write(header.replace('>',''))
-		outfile.write('\t')
-		
-		outfile.write(sample[0])
-		for i in sample[1:]:
-			outfile.write('_')
-			outfile.write(i)
-		outfile.write('\n')
-		
-	else: pass
-fastas.close()	
-outfile.close()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="creates a mothur-formatted "
+        "group file from a fasta file where the sample names are in the "
+        "sequence ids")
+    parser.add_argument('fasta',
+                        type=io_check,
+                        help="input fasta file")
+    args = parser.parse_args()
+    infile = args.fasta
+    outfile = os.path.basename(infile) + '.group'
+
+    r = re.compile("(?<=[Ss][Ee][Rr][Pp]_)(?P<name>SE_\w+)(?=\|)")
+    with open(infile, 'rU') as in_h:
+        with open(outfile, 'w') as out_h:
+            for line in in_h:
+                if line.startswith('>'):
+                    header = line.strip('>\n')
+                    header = header.split()[0]
+                    matched = r.search(header)
+                    if not matched:
+                        print("error: sequence ids are not formatted in such a way that this program can parse")
+                        sys.exit(1)
+                    sample = matched.group('name')
+                    out_h.write("{}\t{}\n".format(header, sample))
+                else:
+                    continue 
+    sys.exit(0)
