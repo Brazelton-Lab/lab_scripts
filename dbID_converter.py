0929190f78932c818046d11081c4df565521946e
Alex Hyer <theonehyer@gmail.com>
2015-06-05 09:50:54 -0600

    Added dbID_converter.py

---
+++ dbID_converter.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+
+'''convert between database ids
+
+THIS PROGRAM IS UNDER CONSTRUCTION AND CURRENTLY DOESN't DO WHAT IT CLAIMS
+To DO.
+'''
+
+__authors__ = 'Alex Hyer'
+__email__ = 'theonehyer@gmail.com'
+__version__ = '0.0.0.2'
+
+import argparse
+from collections import defaultdict
+from __future__ import print_function
+import math
+import os
+import sys
+
+class MyParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: ' + message)
+        self.print_help()
+        sys.exit(2)
+
+def append_table(conversion_files, table_file, annotation_file):
+    '''creates a table to allow for easy conversion between database IDs
+
+    Input:
+
+        conversion_files:
+                A list of files from M5nr of the file name "*.md52id2func".
+                Each file must have three columns containg the md52 checksum,
+                database-specific id, and annotation on the gene respecitvely.
+
+        table_file:
+                The conversion table file to append the converesion entries to
+
+        annotation_file:
+                The annotation file to append annotations to
+
+    Output:
+            Appends conversion entries to conversion table and annotations
+            to the annotation file from the conversion files
+    '''
+
+def create_table(conversion_files, out_table, out_annotation):
+    '''creates a table to allow for easy conversion between database IDs
+
+    Input:
+
+        conversion_files:
+                A list of files from M5nr of the file name "*.md52id2func".
+                Each file must have three columns containg the md52 checksum,
+                database-specific id, and annotation on the gene respecitvely.
+
+        out_table:
+                File to conversion write table to
+
+        out_annotation:
+                File to write annotation table to
+
+    Output:
+
+            Two table files: one consisting of all the different database
+            identifiers for a given gene and another containing the M5nr
+            identifier and annotation for each gene.
+
+    Note: This function can be very RAM intensive.
+    '''
+
+    split = str.split
+    header = ['md5']
+    annotation = ''
+    numberOfFiles = len(conversion_files)
+    identifiers = defaultdict(lambda: [annotation] + \
+                              ['None' for i in range(numberOfFiles)])
+    for file in enumerate(conversion_files):
+        db = split(file[1], '.')[0]
+        db = split(db, r'/')[1]
+        header.append(db)
+        split = str.split
+        with open(file[1], 'rU') as in_handle:
+            for line in in_handle:
+                # [:-1] is to leave out the database name from M5nr
+                cols = split(line[:-1], '\t')
+                md5 = cols[0]
+                dbID = cols[1]
+                annotation = cols[2:]
+                identifiers[md5][file[0]] = dbID
+    with open(out_table, 'w') as conversion_handle:
+        headerStr = '\t'.join(header) + '\n'
+        conversion_handle.write(headerStr)
+        with open(out_annotation, 'w') as annotation_handle:
+            annHeader = 'md5\tannotation\n'
+            annotation_handle.write(annHeader)
+            for key in identifiers.keys():
+                annotation = identifiers[key][0]
+                dbIDs = key + '\t' + '\t'.join(identifiers[key][1:]) + '\n'
+                annotation_output = key + '\t' + ' '.join(annotation) + '\n'
+                conversion_handle.write(dbIDs)
+                annotation_handle.write(annotation_output)
+
+def read_table(table_file, key_db):
+    '''reads in the table file as a nested dictionary and returns the dictionary
+
+    Input:
+
+        table_file:
+                The table file to be read as a dictionary
+
+        key_db:
+                Which database values to use as the key 
+
+    Output:
+            A dictionary where the each key is an entry in the database
+            specified by key_db. Each value is a dictionary where each
+            key is a different database and each value is the equivalent
+            database ID to the parent dictionary database ID.
+
+    '''
+
+    table = {}
+    with open(table_file, 'rU') as in_handle:
+        firstLine = in_handle.readline()
+        splitFirstLine.strip().split('\t')
+        if key_db in splitFirstLine:
+            keyColumn = splitFirstLine.index(key_db)
+        else:
+            print('{} not in {}').format(key_db, table_file)
+            print('Databases in {} follow:').format(table_file)
+            databases = '\n'.join(splitFirstLine)
+            print(databases)
+            sys.exit(1)
+        for line in in_handle:
+            temp = {}
+            lineSplit.strip().split('\t')
+            dbKey = lineSplit[keyColumn]
+            if dbKey != 'None':
+                for id in enumerate(lineSplit):
+                    temp[splitFirstLine[lineSplit[0]]] = lineSplit[1]
+                table[dbKey] = temp
+    return table
+            
+
+def format_create_table_args(args):
+    '''Formats args to fit generalized conversion table related functions'''
+    
+    if args.create:
+        create_table(args.create, args.conversionTable, args.annotationTable)
+    elif args.append:
+       append_table(args.append, args.conversionTable, args.annotationTable)
+
+if __name__ == '__main__':
+    parentParser = MyParser(description = __doc__,
+                            formatter_class = argparse.\
+                            RawDescriptionHelpFormatter)
+    parentParser.add_argument('--version',
+                              action = 'version',
+                              version = '0.0.0.2')
+    subParsers = parentParser.add_subparsers(title = 'sub-commands')
+
+    createTableParser = subParsers.add_parser('createTable',
+                                               help = 'creates conversion '+\
+                                                      'table between databases')
+    createTableParser.add_argument('conversionTable',
+                                    help = 'conversion table to be created '+\
+                                           'or appended to')
+    createTableParser.add_argument('annotationTable',
+                                    help = 'annotation table for conversion table')
+    createTableParser.add_argument('--create',
+                                    nargs = '+',
+                                    help = 'create new conversion table')
+    createTableParser.add_argument('--append',
+                                    nargs = '+',
+                                    help = 'add database conversion to table')
+    createTableParser.set_defaults(func=format_create_table_args)
+
+    convertIdFile = subParsers.add_parser('convertIdFile',
+                                      help = 'converts one database ID to '+\
+                                             'another')
+    convertIdFile.add_argument('in_file',
+                           help = 'The character delimited file to convert '+\
+                                  'database IDs in')
+    convertIdFile.add_argument('out_file',
+                           help = 'File to print results to')
+    convertIdFile.add_argument('column',
+                           help = 'the column of the file containing the '+\
+                                  'database IDs to convert with 0-indexing')
+    convertIdFile.add_argument('originalDB',
+                           help = 'the database type to convert from')
+    convertIdFile.add_argument('targetDB',
+                           help = 'the database type to convert to')
+    convertIdFile.add_argument('--delimiter',
+                           default = '\t',
+                           help = 'the character delimiting the in_file, '+\
+                                  'default is "\\t"')
+    convertIdFile.add_argument('--log_file',
+                           help = 'log file to redirect output to')
+    args = parentParser.parse_args()
+    args.func(args)
+    sys.exit(0)
