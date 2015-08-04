b145f5f6b6b40060cf93ad2e6ccf68da3df09668
Alex Hyer <theonehyer@gmail.com>
2015-08-04 12:41:10 -0600

    Restored create_sub_fasta.py to a more simple state

--- create_sub_fasta.py
+++ create_sub_fasta.py
@@ -20,13 +20,6 @@ Required Arguments:
 
 Optional Arguments:
 
-    --coverage:    Obtain coverages for the obtained FASTA or FASTQ files.
-                   The syntax for this option is as follows:
-                   [coverage_table] [GFF file for first input file]
-                   [GFF file for second input file] ...
-                   WARNING: This is a very specific usage case made for
-                   a very specific request. It will NOT give you entry
-                   coverage in general. For more info, see Alex Hyer.  
     --fastq:       Specifies input files as FASTQ files [Default: FASTA]
 """
 
@@ -40,37 +33,6 @@ import sys
 
 __author__ = 'Alex Hyer'
 __version__ = '0.0.0.4'
-
-
-def append_coverages(entries, table, gff3_files):
-    return_entries = []
-    for file in gff3_files:
-        id_converion_dict = create_id_conversion_dict(file)
-        database = 'Subbio' + gff3_file.split('.')[0].split(r'/')[-1]
-        conversion_table = read_conversion_table(table, database)
-        for entry in entries:
-            prokka_id = entry.split('\n')[0].split('>')[-1].split(' ')[0]
-            if prokka_id in id_conversion_dict:
-                contig_id = id_conversion_dict[prokka_id]
-                if contig_id in conversion_table:
-                    coverage = conversion_table[contig_id]
-                    entry_name = entry.split('\n', 1)[0]
-                    entry_rest = entry.split('\n', 1)[1]
-                    entry_name = entry_name + ' ' + coverage + '\n'
-                    new_entry = entry_name + entry_rest
-                    return_entries.append(new_entry)
-    return return_entries
-
-
-def create_id_conversion_dict(gff3_file):
-    temp_dict = {}
-    with open(gff3_file, 'rU') as gff3_handle:
-        for entry in gff3_iter(gff3_handle):
-            contig_id = entry['seqid']
-            prokka_id = entry['attributes'].lstrip('ID=').split(';')[0]
-            if prokka_id not in temp_dict:
-                temp_dict[prokka_id] = contig_id
-    return temp_dict
 
 
 def compile_ids(ids):
@@ -121,26 +83,6 @@ def extract_ids(ids, files, fastq=False):
     return entries
                     
 
-def read_conversion_table(conversion_table_file, database):
-    temp_dict = {}
-    with open(conversion_table_file, 'rU') as table_handle:
-        first_line = table_handle.readline()
-        databases = first_line.strip().split('\t')
-        if database in databases:
-            column_number = databases.index(database)
-        else:
-            print('{0} not in {1}. Avaialble databases are:'.format(
-                  database, conversion_table_file))
-            print('\n'.join(databases[1:]))
-        for line in table_handle:
-            columns = line.strip().split('\t')
-            contig_id = columns[0]
-            coverage = columns[column_number]
-            if coverage != 'None':
-                temp_dict[contig_id] = coverage
-    return temp_dict
-        
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class=argparse.
@@ -149,13 +91,9 @@ if __name__ == '__main__':
                         default=None,
                         nargs='?',
                         help='name of FASTA or FASTQ file to write')
-    parser.add_argument('--coverage',
-                        default=False,
-                        help='obtain coverage for FASTA or FASTQ entries,' \
-                             ' see help for more details')
     parser.add_argument('--fastq',
                         default=False,
-                        nargs='*',
+                        action='store_true',
                         help='specify input file as FASTQ file [Default: FASTA]')
     parser.add_argument('--files',
                         default=None,
@@ -163,6 +101,7 @@ if __name__ == '__main__':
                         help='Files to extract IDs from')
     parser.add_argument('--ids', metavar='IDs',
                         default=None,
+                        nargs='*',
                         help='IDs to extract from input files')
     args = parser.parse_args()
 
@@ -174,11 +113,6 @@ if __name__ == '__main__':
         sys.exit(1)
     else:
         entries = extract_ids(args.ids, args.files, fastq=args.fastq)
-        if not args.coverage is None:
-            print('WARNING: See help before using the coverage option')
-            table = args.coverage[0]
-            gff3_files = args.coverage[1:]
-            entries = append_coverages(entries, table, gff3_files)
 
     with open(args.output, 'w') as out_handle:
         for entry in entries:
