b94a57419f00668be7a09117d7381afce7f00d9d
Christopher Thornton <christopher.n.thornton@gmail.com>
2016-03-18 12:38:36 -0600

    add table_from_humann2.py

---
+++ table_from_humann2.py
@@ -0,0 +1,95 @@
+#! /usr/bin/env python
+"""
+Create a multiple sample table of Pathway Abundances using individual sample 
+pathabundance files from HUMAnN2
+"""
+
+from __future__ import print_function
+import argparse
+import sys
+import os
+
+def print_path(pathway, database, size):
+    abundances = database[pathway]
+    diff = size - len(abundances)
+    if diff > 0:
+        abundances.extend(list('0') * diff)
+    print("{}\t{}".format(pathway, '\t'.join(abundances)))
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('infiles', metavar='pathabundance',
+        nargs='+',
+        help="space-separated list of HUMAnN2 pathabundance files")
+
+    parser.add_argument('-l', '--labels', metavar='"label, label, ..."',
+        type=str,
+        help="sample names to use in the report, given in a comma-separated "
+            "list [default: use file names]. The order given must match the "
+            "order in which the files are provided. If spaces are used "
+            "in-between labels, the full argument must be wrapped in quotes.")
+
+    args = parser.parse_args()
+
+    labels = args.labels.split(',')
+    labels = [i.lstrip() for i in labels]
+
+    valid_files = []
+    # check that the can be opened
+    for index, infile in enumerate(args.infiles):
+        infile = args.infiles[index]
+        try:
+            fh = open(infile).close()
+        except IOError:
+            basename = os.path.basename(infile)
+            print("Unable to open {} ... skipping".format(basename), file=sys.stderr)
+            if labels:
+                labels = labels[:index] + labels[index + 1:]
+        else:
+            valid_files.append(infile)
+
+    path_db = {}
+
+    for index, infile in enumerate(valid_files):
+        with open(infile) as in_h:
+            for line in in_h:
+                if line.startswith('#'):
+                    continue
+                try:
+                    pathway, abundance = line.strip().split('\t')
+                except ValueError:
+                    basename = os.path.basename(infile)
+                    print("file '{}' does not have a recognizable format".format(basename), file=sys.stderr)
+                    sys.exit(1)
+                if pathway in path_db:
+                    diff = index - len(path_db[pathway])
+                    if diff > 0:
+                        path_db[pathway].extend(list('0') * diff)
+                    path_db[pathway].append(abundance)
+                else:
+                    path_db[pathway] = list('0') * index + [abundance]
+
+    num_valid = len(valid_files)
+
+    if labels:
+        header = "# Pathway\t{}".format('\t'.join(labels))
+    else:
+        header = "# Pathway\t{}".format('\t'.join(valid_files))
+    print(header)
+
+    if 'UNMAPPED' in path_db:
+        print_path('UNMAPPED', path_db, num_valid)
+
+    if 'UNINTEGRATED' in path_db:
+        print_path('UNINTEGRATED', path_db, num_valid)
+
+    for pathway in sorted(path_db):
+        if pathway == 'UNMAPPED' or pathway == 'UNINTEGRATED':
+            continue
+        print_path(pathway, path_db, num_valid)
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
