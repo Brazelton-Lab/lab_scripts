009764da845a070bf446f7bfb21966ae026235d9
Billy Brazelton <bbrazelton@gmail.com>
2016-04-25 22:10:50 -0600

    fixed bug

--- strip-prokka-suffix.py
+++ strip-prokka-suffix.py
@@ -5,7 +5,7 @@
 # will run on any .gff file in same directory
 
 import glob
-for filename in glob.glob(*.gff):
+for filename in glob.glob('*.gff'):
 	with open(filename) as gff:
 		newfilename = filename.replace('.gff','.good.gff')
 		print 'writing' + newfilename
