a5ec5f772906678fa3c1f2055499374be817b0e8
Alex Hyer <theonehyer@gmail.com>
2016-06-08 16:00:46 -0600

    Debug change to get_sub_fasta.py

--- get_sub_fasta.py
+++ get_sub_fasta.py
@@ -66,7 +66,7 @@ def extract_ids(ids, files, fastq=False):
     entries = []
     compiled_ids = compile_ids(ids)
     for file_ in files:
-        with open(file_, 'rU') as in_handle
+        with open(file_, 'rU') as in_handle:
             for entry in fastaq_iter(in_handle, fastq=fastq):
                 for compiled_id in compiled_ids:
                     if len(compiled_id.findall(entry['name'])) == 1:
