1da930c851453f68ebca80c67bae34011d43623e
Alex Hyer <theonehyer@gmail.com>
2016-06-08 16:40:30 -0600

    Debug change to get_sub_fasta.py

--- get_sub_fasta.py
+++ get_sub_fasta.py
@@ -69,7 +69,6 @@ def extract_ids(ids, files, fastq=False):
         with open(file_, 'rU') as in_handle:
             for entry in fastaq_iter(in_handle, fastq=fastq):
                 for compiled_id in compiled_ids:
-                    print(len(compiled_id.findall(entry['name'])))
                     if len(compiled_id.findall(entry['name'])) == 1:
                         to_return = ''
                         if not fastq:
@@ -81,6 +80,7 @@ def extract_ids(ids, files, fastq=False):
                                                              entry['sequence'],
                                                              entry['accuracy'])      
                         # Stop after first ID match
+                        print(to_return)
                         break
                         entries.append(to_return)
     return entries
