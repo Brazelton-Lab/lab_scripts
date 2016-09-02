a7be48f92e39f570eb173cb4c3013423dcfbb4dd
TheOneHyer <theonehyer@gmail.com>
2016-09-02 10:24:27 -0600

    Modified get-sub-prokka.py to be Intuitive
    get-sub-prokka.py now jsut changes the file
    ending instead on mangling the whole filename

--- get-sub-prokka.py
+++ get-sub-prokka.py
@@ -14,11 +14,11 @@ ffn_file = sys.argv[4]
 if gff_file[-4:] == '.gff': pass
 if faa_file[-4:] == '.faa': pass
 if ffn_file[-4:] == '.ffn': pass
-else: 
+else:
 	print 'Please enter files in this order: ESOM_bin.fasta assembly.gff assembly.faa assembly.ffn'
 	sys.exit()
 
-root = contigfile.split('.')
+root = contigfile.split('.')[:-1]
 root = root[0].split('/')
 root = root[-1]
 newfaafile = root + '.faa'
@@ -26,8 +26,8 @@ newffnfile = root + '.ffn'
 
 c = []
 with open(contigfile) as contigs:
-	for line in contigs: 
-		if line[0] == '>': 
+	for line in contigs:
+		if line[0] == '>':
 			header = line.strip('>')
 			header = header.replace('-','_')
 			header = header.replace(' ','_')
@@ -40,7 +40,7 @@ with open(gff_file) as gff:
 	for line in gff:
 		if '##FASTA'in line: break
 		if line[0] == '#': pass
-		else: 
+		else:
 			contig_id = line.split('\t')
 			contig_id = contig_id[0].replace('-','_')
 			if contig_id in c:
@@ -53,7 +53,7 @@ count = 0
 from Bio import SeqIO
 with open(newfaafile,'w') as newfaa:
 	for faa in SeqIO.parse(faa_file,'fasta'):
-		if faa.id in p: 
+		if faa.id in p:
 			SeqIO.write(faa,newfaa,'fasta')
 			count = count + 1
 
@@ -61,11 +61,11 @@ print count,
 print 'entries from .faa file written to',
 print newfaafile
 count = 0
-	
+
 from Bio import SeqIO
 with open(newffnfile,'w') as newffn:
 	for ffn in SeqIO.parse(ffn_file,'fasta'):
-		if ffn.id in p: 
+		if ffn.id in p:
 			SeqIO.write(ffn,newffn,'fasta')
 			count = count + 1
 
