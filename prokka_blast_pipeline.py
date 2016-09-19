236292e3c1a17b182dbc065235e094288f490db8
TheOneHyer <theonehyer@gmail.com>
2016-09-19 16:27:33 -0600

    Added prokka_blast_pipeline
    Added skeleton of new script to BLAST
    PROKKA annotations.

---
+++ prokka_blast_pipeline.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+
+"""BLAST sequences from PROKKA annotations
+
+
+"""
+
+import argparse
+
+__author__ = 'Alex Hyer'
+__email__ = 'theonehyer@gmail.com'
+__license__ = 'GPLv3'
+__maintainer__ = 'Alex Hyer'
+__status__ = 'Alpha'
+__version__ = '0.0.1'
+
+if __name__ == '__main__':
+    pass
+
