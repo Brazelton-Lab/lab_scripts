bdbaa43da40b17639f5256b567eea46a27400cdc
TheOneHyer <theonehyer@gmail.com>
2016-09-19 16:42:43 -0600

    Added Argparse to prokka_blast_pipeline
    prokka-blast_pipeline now has putative CML
    arguments.

--- prokka_blast_pipeline.py
+++ prokka_blast_pipeline.py
@@ -14,8 +14,37 @@ __email__ = 'theonehyer@gmail.com'
 __license__ = 'GPLv3'
 __maintainer__ = 'Alex Hyer'
 __status__ = 'Alpha'
-__version__ = '0.0.1'
+__version__ = '0.0.1a2'
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.
+                                     RawDescriptionHelpFormatter)
+    parser.add_argument('-e', '--e_value',
+                        default=10.0,
+                        type=float,
+                        help='Maximum E-Value of alignment permitted')
+    parser.add_argument('-f', '--fna',
+                        required=True,
+                        type=argparse.FileType,
+                        help='FNA file from PROKKA containing nucleotide '
+                             'sequences of annotated proteins')
+    parser.add_argument('-i', '--id',
+                        required=True,
+                        type=argparse.FileType,
+                        help='line-separated list of IDs designating which '
+                             'PROKKA IDs to BLAST with')
+    parser.add_argument('-p', '--program',
+                        default='blastn',
+                        type=str,
+                        choices=[
+                            'blastn',
+                            'blastx',
+                            'tblastx'
+                        ],
+                        help='BLAST+ program to search with')
+    parser.add_argument('-t', '--top',
+                        default=True,
+                        action='store_true',
+                        help='only return top BLAST result')
 
