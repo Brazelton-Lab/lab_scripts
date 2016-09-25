9a6c97a8108b18cf28394ff9a52b35dd49f0b039
TheOneHyer <theonehyer@gmail.com>
2016-09-25 15:52:26 -0600

    Added ArgParse to anvi_coverter
    anvi_converter now has most of its ArgumentParser
    complete. More skeletal elements (main function).

--- anvi_converter.py
+++ anvi_converter.py
@@ -7,6 +7,8 @@ from __future__ import print_function
 
 """
 
+import argparse
+from bio_utils.iterators import fasta_iter, gff3_iter
 import sys
 
 __author__ = 'Alex Hyer'
@@ -14,7 +16,7 @@ __email__ = 'theonehyer@gmail.com'
 __license__ = 'GPLv3'
 __maintainer__ = 'Alex Hyer'
 __status__ = 'Alpha'
-__version__ = '0.0.1'
+__version__ = '0.0.1a2'
 
 
 def main(args):
@@ -28,5 +30,37 @@ def main(args):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.
+                                     RawDescriptionHelpFormatter)
+    subparsers = parser.add_subparsers(title='Tool',
+                                       dest='tool')
+
+    bins = subparsers.add_parser('bins',
+                                 help='read several FASTA files and produce '
+                                      'TSV relating entries to file name')
+    bins.add_argument('FASTA files',
+                      dest='fasta_files',
+                      type=list,
+                      help='list of space-separated FASTA files where each '
+                           'file is a bin')
+    bins.add_argument('output',
+                      type=argparse.FileType('w'),
+                      help='output file')
+
+    prokka = subparsers.add_parser('PROKKA',
+                                   dest='prokka',
+                                   help='convert GFF3 file from Christopher '
+                                        'Thornton\'s modified version of '
+                                        'PROKKA into two TSVs containing '
+                                        'called gene locations and their '
+                                        'annotations')
+    prokka.add_argument('GFF3',
+                        dest='gff3',
+                        type=argparse.FileType('r'),
+                        help='GFF3 file to convert')
+    prokka.add_argument('prefix',
+                        type='str',
+                        help='prefix for output files')
 
     sys.exit(0)
