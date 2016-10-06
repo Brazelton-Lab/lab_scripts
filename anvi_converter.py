6d4aaca29671fe2c0e6dce978d68472da7448446
TheOneHyer <theonehyer@gmail.com>
2016-10-06 17:02:43 -0600

    Added Header to Gene_Locations File

--- anvi_converter.py
+++ anvi_converter.py
@@ -18,7 +18,7 @@ __email__ = 'theonehyer@gmail.com'
 __license__ = 'GPLv3'
 __maintainer__ = 'Alex Hyer'
 __status__ = 'Production'
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 
 def main(args):
@@ -45,6 +45,10 @@ def main(args):
             for entry in gff3_iter(args.GFF3):
                 if entry.type == 'CDS' and \
                         'gene' in entry.attributes.keys():
+
+                    lh.write('gene_callers_id\tcontig\tstart\tstop\t'
+                             'direction\tpartial\tsource\tversion{0}'
+                             .format(os.linesep))
 
                     # Reformat data for gene locations file
                     direction = 'f' if entry.strand == '+' else 'r'
