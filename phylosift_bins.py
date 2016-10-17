460b6dc81e1721cbfb795212ee2193a71d7c6d8e
TheOneHyer <theonehyer@gmail.com>
2016-10-17 16:08:09 -0600

    Fixed File Naming in phylosift_bins
    phylosift_bins incorrectly named output files: recitfied.

--- phylosift_bins.py
+++ phylosift_bins.py
@@ -19,7 +19,7 @@ __email__ = 'theonehyer@gmail.com'
 __license__ = 'GPLv3'
 __maintainer__ = 'Alex Hyer'
 __status__ = 'Alpha'
-__version__ = '0.0.1a6'
+__version__ = '0.0.1a7'
 
 
 def main(args):
@@ -41,7 +41,7 @@ def main(args):
     for fasta in args.fasta:
         with open(fasta, 'r') as fasta_handle:
             for entry in fasta_iter(fasta_handle):
-                out_file = os.sep.join(fasta.name.split('.')[:-1])
+                out_file = os.sep.join(fasta_handle.name.split('.')[:-1])
                 out_name = os.path.abspath(args.output_dir + out_file)
                 with open(out_name) as out_handle:
                     out_handle.write('#Sequence_ID\tHit_Coordinates\t'
