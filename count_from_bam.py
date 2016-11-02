b581d437fffc870fc3db6c99f4a1fb726efabbfa
TheOneHyer <theonehyer@gmail.com>
2016-11-02 15:50:18 -0600

    count_from_bam: added shell and todo
    Added basic metadata to new program
    count_from_bam

---
+++ count_from_bam.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+"""
+
+
+"""
+
+__author__ = 'Alex Hyer'
+__email__ = 'theonehyer@gmail.com'
+__license__ = 'GPLv3'
+__maintainer__ = 'Alex Hyer'
+__status__ = 'Alpha'
+__version__ = '0.0.1a1'
+
+# TODO: write program
+
