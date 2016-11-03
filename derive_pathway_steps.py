d2948f5cdd2d4b317726def035656dba58efe2b0
TheOneHyer <theonehyer@gmail.com>
2016-11-03 12:37:46 -0600

    derive_pathway_steps: Added Metadata
    Added metadata of new script. Script unwritten.

---
+++ derive_pathway_steps.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+"""
+
+Copyright:
+
+    derive_pathway+steps.py Obtain gene list from pathway databases
+    Copyright (C) 2016  William Brazelton, Alex Hyer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = 'Alex Hyer'
+__email__ = 'theonehyer@gmail.com'
+__license__ = 'GPLv3'
+__maintainer__ = 'Alex Hyer'
+__status__ = 'Alpha'
+__version__ = '0.0.1a1'
+
