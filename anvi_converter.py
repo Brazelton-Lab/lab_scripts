bf17c949b6d99b2bfaba1adf8f86a3755175b4f6
TheOneHyer <theonehyer@gmail.com>
2017-07-13 16:56:25 -0600

    Added Gene Filter to anvi_converter

--- anvi_converter.py
+++ anvi_converter.py
@@ -36,7 +36,7 @@ __email__ = 'theonehyer@gmail.com'
 __license__ = 'GPLv3'
 __maintainer__ = 'Alex Hyer'
 __status__ = 'Production'
-__version__ = '1.1.6'
+__version__ = '1.1.7'
 
 
 def main(args):
