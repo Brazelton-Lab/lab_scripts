d53861d2fa159863a751fd91871d4dfdc5f4bca5
Billy Brazelton <bbrazelton@gmail.com>
2018-04-03 17:31:16 -0600

    new script for zeroing out taxa in a sample

---
+++ 16S/zero-taxa.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+# zero-out taxa for a user-provided specific sample 
+# might be useful for removing taxa from some samples (but not others) prior to running sourcetracker, for example
+# the script will find taxa in the provided count table that match the first column of a user-provided file
+# assumes files are csv
+
+import sys
+infilename = sys.argv[1]
+listfilename = sys.argv[2]
+sample = sys.argv[3]
+outfilename = infilename.replace('.csv','.zeros.csv')
+
+with open(listfilename) as listfile:
+	l = []
+	for row in listfile:
+		row = row.split(',')
+		i = row[0]
+		l.append(i.strip('\n'))
+
+with open(outfilename, 'w') as outfile:
+	with open(infilename) as infile:
+		for row in infile:			
+			outfile.write(row)		# write the first line
+			s = []
+			cols = row.split(',')
+			for col in cols:
+				s.append(col)		# store list of sample names
+			break
+		
+		if sample in s:
+			pos = s.index(sample)
+		else: 
+			print 'error:',
+			print sample, 
+			print 'not found in',
+			print infilename
+			sys.exit()
+			
+		for row in infile:
+			cols = row.split(',')
+			j = cols[0]
+			if j in l:
+				outfile.write(j)
+				for col in cols[:pos]: outfile.write(',' + col)
+				outfile.write(',0')
+				for col in cols[pos+1:]: outfile.write(',' + col)
+			else: outfile.write(row)
