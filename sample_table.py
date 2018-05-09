dfccdd2db7228f77b2e604bd43a29b0a0b87719c
Christopher Thornton <christopher.n.thornton@gmail.com>
2018-05-09 11:31:52 -0600

    sample_tables.py: verify format of sample ID

--- sample_table.py
+++ sample_table.py
@@ -27,17 +27,24 @@ from __future__ import print_function
 import argparse
 from datetime import date
 import os
+import re
 import sys
 from time import sleep
 
 
 def parse_commas(argument):
+    """Return list from command line argument with values separated by a comma.
+    """
     argument = argument.strip().replace(' ' , '')
     argument = argument.split(',')
     return(argument)
 
 
-def get_new_id(infile, pref_len=2, sep=','):
+def get_new_id(infile, prefix, sep=','):
+    """return the next number in the series from the most recent identifying
+    number in the master table."""
+    ID = re.compile("{}[0-9]+".format(prefix))
+
     with open(infile) as in_h:
         header = in_h.readline()
         header = header.split(sep)
@@ -46,16 +53,30 @@ def get_new_id(infile, pref_len=2, sep=','):
         except ValueError:
             print("Error: no column named 'SampleID' found in {}"\
                   .format(infile), file=sys.stderr)
+            leave = input('Press any key to exit')
             sys.exit(1)
 
         samples = []
         for line in in_h:
             line = line.strip().split(sep)
             sample = line[index]
+
+            # Verify that the format of the sample ID matches what is expected
+            if not ID.match(sample):
+                print("Error: sample IDs must consist of one or more "
+                      "alphabetical characters followed by one or more "
+                      "integers", file=sys.stderr)
+                leave = input('Press any key to exit')
+                sys.exit(1)
+
+            # Store ID numbers
             try:
-                sample_number = int(sample[pref_len:])
+                sample_number = int(sample[len(prefix):])
             except ValueError:
-                print("Error: sample IDs must consist of two alphabetical characters followed by one or more integers", file=sys.stderr)
+                print("Error: sample IDs must consist of one or more "
+                      "alphabetical characters followed by one or more "
+                      "integers", file=sys.stderr)
+                leave = input('Press any key to exit')
                 sys.exit(1)
             samples.append(sample_number)
 
@@ -71,18 +92,23 @@ def main():
         formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-m', '--master',
         default='C:\\Users\\user\Documents\LC2018\LC_2018.sample_table.master.csv',
-        help="master sample table [default: C:\\Users\\user\Documents\LC2018\LC_2018.sample_table.master.csv]")
+        help="master sample table [default: C:\\Users\\user\Documents\LC2018\\"
+             "LC_2018.sample_table.master.csv]")
     parser.add_argument('-o', '--out_dir',
         default='C:\\Users\\user\Desktop\\',
         help="output directory [default: C:\\Users\\user\Desktop\]")
     parser.add_argument('-i', '--id',
         type=int,
-        help="first sample ID number to be assigned to the new table. Ignores the last sample ID number found in the master table")
+        help="first sample ID number to be assigned to the new table. Ignores "
+             "the last sample ID number found in the master table")
     parser.add_argument('-f', '--fields',
         dest='fields',
-        default="CruiseID,PrivateID,ContainerType,SampleType,SampleQuantity,StorageMethod",
+        default="CruiseID,PrivateID,ContainerType,SampleType,SampleQuantity,\
+                 StorageMethod",
         type=parse_commas,
-        help="comma-separated list of additional field names to include in the sample table [default: CruiseID, PrivateID, ContainerType, SampleType, SampleQuantity, StorageMethod]")
+        help="comma-separated list of additional field names to include in "
+             "the sample table [default: CruiseID, PrivateID, ContainerType, "
+             "SampleType, SampleQuantity, StorageMethod]")
     parser.add_argument('-s', '--sep',
         default=',',
         help="field separator character [default: ,]")
@@ -110,6 +136,7 @@ def main():
     if args.sep in assignee:
         print("Error: name '{}' cannot contain the same character '{}' that "
               "is used as the field separator".format(assignee, args.sep))
+        leave = input('Press any key to exit')
         sys.exit(1)
 
     nlabel = input('Enter the desired number of labels: ')
@@ -119,10 +146,11 @@ def main():
     except ValueError:
         print("Error: number of labels must be an integer value", \
               file=sys.stderr)
+        leave = input('Press any key to exit')
         sys.exit(1)
 
     # Find the sample ID range
-    first_id = get_new_id(args.master, pref_len=len(args.code), sep=args.sep)
+    first_id = get_new_id(args.master, args.code, sep=args.sep)
     last_id = first_id + nlabel
     sample_range = "{}-{}".format(first_id, last_id - 1)
 
@@ -130,16 +158,20 @@ def main():
     output = "{}{}_{}_{}.csv".format(args.out_dir, assignee.replace(" ", "_"), \
              sample_range, current_date)
 
-    print("Outputting table of sample IDs in {}".format(output), file=sys.stdout)
+    print("Outputting table of sample IDs in {}".format(output), \
+          file=sys.stdout)
     nfields = len(args.fields)
     ncomma = "," * nfields
     with open(output, 'w') as out_h, open(args.master, 'a') as master_h:
         out_h.write('SampleID,Owner,Date,{}\n'.format(','.join(args.fields)))
         for sid in list(range(first_id, last_id)):
-            out_h.write('LC{},{},{}{}\n'.format(str(sid).zfill(args.pad), assignee, current_date, ncomma))
-            master_h.write('LC{},{},{}\n'.format(str(sid).zfill(args.pad), assignee, current_date))
+            out_h.write('LC{},{},{}{}\n'.format(str(sid).zfill(args.pad), \
+                        assignee, current_date, ncomma))
+            master_h.write('LC{},{},{}\n'.format(str(sid).zfill(args.pad), \
+                           assignee, current_date))
 
-    print("Identifying number of first sample in series: {}".format(str(first_id).zfill(args.pad)), file=sys.stdout)
+    print("Identifying number of first sample in series: {}"\
+          .format(str(first_id).zfill(args.pad)), file=sys.stdout)
 
     # Allow time to note ID number before exiting
     sleep(2)
