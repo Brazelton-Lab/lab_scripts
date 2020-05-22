97ec64929f6a26d736a8ff427b4c3801a572aa42
Billy Brazelton <bbrazelton@gmail.com>
2020-05-22 15:51:42 -0600

    new script

---
+++ genbank_to_faa.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+# export FASTA file of CDS protein sequences from a multi-record genbank file
+# borrowed some from Cedar McKay's script (Rocap lab)
+
+import sys
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+import glob
+for genbankfilename in glob.glob("*.gb*"):
+	with open(genbankfilename) as g:
+		if "LOCUS " in g.readline():
+			fastafilename = genbankfilename + '.faa'
+			with open(fastafilename, 'w') as f:
+				for seq_record in SeqIO.parse(g, "genbank"):
+					definition =  seq_record.description
+					i = 0
+					for feature in seq_record.features:
+						if feature.type=="CDS":
+							i = i + 1
+							id = definition + '_' + str(i)
+							product = feature.qualifiers['product'][0]
+			
+							# stole location parsing from Cedar McKay
+							#Go through some pain to make location human readable by adding 1 to first position
+							location = str(int(str(feature.location.nofuzzy_start))+1) + ":" + str(feature.location.nofuzzy_end) 
+							if feature.strand == 1:
+								location = location + ' Forward'
+							elif feature.strand == -1:
+								location = location + ' Reverse'
+							else:
+								location = location + ' Could not determine strand'
+			
+							seq = Seq(feature.qualifiers['translation'][0])
+							header = id + ' ' + product + ' ' + location
+							newrecord = SeqRecord(seq, header, description='')
+							SeqIO.write(newrecord,f,'fasta')
+						else: pass
+		else: pass
+				
+			
